81ddf698a5976735c72f381e0bf4b607efa9600c
sparsh-otpless <sparsh.chadha@otpless.com>
2024-12-20 17:35:28 +0530

    removed pods

--- Example/Pods/Target Support Files/OtplessSDK/OtplessSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_OtplessSDK : NSObject
-@end
-@implementation PodsDummy_OtplessSDK
-@end
--- Example/Pods/Target Support Files/Pods-OtplessSDK_Tests/Pods-OtplessSDK_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_OtplessSDK_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_OtplessSDK_Tests
-@end
